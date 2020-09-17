8cc4023a5dd81b7fc77721e5bceff2c31ed38e70
Sebastian <lopezsebastian.emanuel@gmail.com>
2020-09-16 21:52:16 -0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE HTML>
+<!--
+	Retrospect by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Tomilones</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<h1><a href="index.html">TOMILONES</a></h1>
+				<a href="#nav">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="nav">
+				<ul class="links">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="generic.php">Generic</a></li>
+					<li><a href="elements.php">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<i class="icon fa-diamond"></i>
+				<h2>¿Ya tienes tu tomilon?</h2>
+				<p>Prueba cada una de sus nuevas caracteristicas.</p>
+				<ul class="actions">
+					<li><a href="#" class="button big special">Saber mas</a></li>
+				</ul>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<div class="inner">
+					<article class="feature left">
+						<span class="image"><img src="images/pic01.jpg" alt="" /></span>
+						<div class="content">
+							<h2>A tomilon le gustan los candados</h2>
+							<p>Si quieres ser un verdadero tomilon fan debes conseguir ya uno de nuestros estupendos candados.</p>
+							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">Mas</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+					<article class="feature right">
+						<span class="image"><img src="images/pic02.jpg" alt="" /></span>
+						<div class="content">
+							<h2>Tomilon suele tomar trenes</h2>
+							<p>Por mas que tengas los mejores candados que ofrecemos, si no te compras un tren, no sos un verdadero tomilon fan.</p>
+							<ul class="actions">
+								<li>
+									<a href="#" class="button alt">Mas</a>
+								</li>
+							</ul>
+						</div>
+					</article>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>Algunos sitios que ha visitado Tomilon</h2>
+						<p>Si el fue, vos deberias ir, salu2.</p>
+					</header>
+					<div class="image-grid">
+						<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+						<a href="#" class="image"><img src="images/pic10.jpg" alt="" /></a>
+					</div>
+					<ul class="actions">
+						<li><a href="#" class="button big alt">Como ir</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style3 special">
+				<div class="inner">
+					<header class="major narrow	">
+						<h2>A tomilon lo queremos mucho</h2>
+						<p>Si tu tambien lo quieres haz clik en el boton de abajo.</p>
+					</header>
+					<ul class="actions">
+						<li><a href="#" class="button big alt">♥</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style2 special">
+				<div class="inner">
+					<header class="major narrow">
+						<h2>Pasale tus datos a tomilon y enviale un mensaje</h2>
+						<p>Vos sabes que queres</p>
+					</header>
+					<form action="#" method="POST">
+						<div class="container 75%">
+							<div class="row uniform 50%">
+								<div class="6u 12u$(xsmall)">
+									<input name="name" placeholder="Nombre" type="text" />
+								</div>
+								<div class="6u$ 12u$(xsmall)">
+									<input name="email" placeholder="Email" type="email" />
+								</div>
+								<div class="12u$">
+									<textarea name="message" placeholder="Mensaje" rows="4"></textarea>
+								</div>
+							</div>
+						</div>
+						<ul class="actions">
+							<li><input type="submit" class="special" value="Enviar" /></li>
+							<li><input type="reset" class="alt" value="Enviar pero en negro" /></li>
+						</ul>
+					</form>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon fa-facebook">
+							<span class="label">Facebook</span>
+						</a></li>
+						<li><a href="#" class="icon fa-twitter">
+							<span class="label">Twitter</span>
+						</a></li>
+						<li><a href="#" class="icon fa-instagram">
+							<span class="label">Instagram</span>
+						</a></li>
+						<li><a href="#" class="icon fa-linkedin">
+							<span class="label">LinkedIn</span>
+						</a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled.</li>
+						<li>Images: <a href="http://unsplash.com">Unsplash</a>.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a>.</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
